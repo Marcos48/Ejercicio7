1e62a165a24078d88498757fcdb59a3803501eee
Marcos48 <marcos.n.vidal@gmail.com>
2015-09-16 00:33:40 +0200

    hola

--- array_utils.rb
+++ array_utils.rb
@@ -1,10 +1,11 @@
 module ArrayUtils
 	def self.array_max(param_array)
-		max = param_array 
+		max = param_array[0]
 
 		param_array.each do |element| 
-			max = element if element > max 
-		end 
+			max = element > max ? element : max
+
+		end
 
 		max 
 
